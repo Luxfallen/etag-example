756ffb627e599f1c0c8e2ef6154dc63b33ceae88
Hawkon Gustavsson <h.gustavsson96@gmail.com>
2017-09-21 15:10:52 -0400

    Worked on class example. Refer to Cody's update on class GitHub.

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -6,23 +6,47 @@ const users = {};
 
 // sha1 is a bit of a quicker hash algorithm for insecure things
 let etag = crypto.createHash('sha1').update(JSON.stringify(users));
-//grab the hash as a hex string
+// grab the hash as a hex string
 let digest = etag.digest('hex');
 
 const respondJSON = (request, response, status, object) => {
-
+  const headers = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const respondJSONMeta = (request, response, status) => {
-
+  const headers = {
+    'Content-Type': 'application/json',
+    etag: digest,
+  };
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
-
+  const responseJSON = {
+    users,
+  };
+  if (request.headers['if-none-match'] === digest) {
+    return respondJSONMeta(request, response, 304);
+  }
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const updateUser = (request, response) => {
+  const newUser = {
+    createdAt: Date.now(),
+  };
+  users[newUser.createdAt] = newUser;
+  etag = crypto.createHash('sha1').update(JSON.stringify(users));
+  digest = etag.digest('hex');
 
+  return respondJSON(request, response, 201, newUser);
 };
 
 const getUsersMeta = (request, response) => {
--- src/server.js
+++ src/server.js
@@ -7,8 +7,32 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
-
-
+  if (request.method === 'GET') {
+    switch (parsedUrl.pathname) {
+      case '/':
+        htmlHandler.getIndex(request, response);
+        break;
+      case '/style.css':
+        htmlHandler.getCSS(request, response);
+        break;
+      case '/getUsers':
+        jsonHandler.getUsers(request, response);
+        break;
+      case '/updateUser':
+        jsonHandler.updateUser(request, response);
+        break;
+      default:
+        jsonHandler.notFound(request, response);
+    }
+  } else if (request.method === 'HEAD') {
+    if (parsedUrl === '/getUsers') {
+      jsonHandler.getUsersMeta(request, response);
+    } else {
+      jsonHandler.notFoundMeta(request, response);
+    }
+  } else {
+    jsonHandler.notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);
